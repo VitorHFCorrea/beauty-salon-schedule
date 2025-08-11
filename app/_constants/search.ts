e7071b5966411037fdadc0b8e47dfc402eba4ce6
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-08-11 13:00:51 -0300

    feat: add BookingItem component

---
+++ app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOptions {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOptions[] = [
+  {
+    imageUrl: "/scissors.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/scissors.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/scissors.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/scissors.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/scissors.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/scissors.svg",
+    title: "Cabelo",
+  },
+]
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
+import { quickSearchOptions } from "./_constants/search"
 
 const Home = () => {
   return (
@@ -21,6 +22,20 @@ const Home = () => {
           </Button>
         </div>
 
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Beleza com hora marcada"
@@ -30,7 +45,10 @@ const Home = () => {
           />
         </div>
 
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="w-fit">Confirmado</Badge>
@@ -45,6 +63,19 @@ const Home = () => {
           </CardContent>
         </Card>
       </div>
+
+      <footer>
+        <Card>
+          <CardContent className="flex flex-col gap-2 px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2025 Copyright <span className="font-semibold">Beauty</span>
+            </p>
+            <p className="text-xs text-gray-400">
+              Powered By <span className="font-semibold">Kahfujii</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
